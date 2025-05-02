5437e5e26ef3cf19aa13ad1eea55c4885531fb15
Alon <alonso.moros@alumnos.upm.es>
2025-05-02 20:59:38 +0200

    Primer commit del proyecto local

---
+++ core/src/main/java/com/alon/pruebasGDX/Main.java
@@ -0,0 +1,35 @@
+package com.alon.pruebasGDX;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+
+    private static final String TITLE = "pruebasGDX";
+    private static final String VERSION = "v.0.0.0.0";
+
+    @Override
+    public void create () {
+
+    }
+
+    @Override
+    public void resize (int width, int height) {
+    }
+
+    @Override
+    public void render () {
+    }
+
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
+    }
+
+    @Override
+    public void dispose () {
+    }
+}
