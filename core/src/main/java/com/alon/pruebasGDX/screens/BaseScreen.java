0e8f06c856b5a0f6689df692ed923ddefcf03d10
Alon <alonso.moros@alumnos.upm.es>
2025-05-03 17:42:45 +0200

    Implementadas clases para control de ventanas, creada padre BaseScreen y MainMenuScreen con la ventana principal

--- core/src/main/java/com/alon/pruebasGDX/screens/BaseScreen.java
+++ core/src/main/java/com/alon/pruebasGDX/screens/BaseScreen.java
@@ -1,4 +1,64 @@
 package com.alon.pruebasGDX.screens;
 
-public class BaseScreen {
+import com.alon.pruebasGDX.Prueba1;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+
+public abstract class BaseScreen implements Screen {
+    protected final Prueba1 game;
+
+    protected OrthographicCamera camera;
+    protected Viewport viewport;
+    protected Stage stage;
+
+    public BaseScreen(Prueba1 game) {
+        this.game = game;
+
+        // Cámara ortográfica para mundo 2D
+        camera = new OrthographicCamera();
+
+        // FitViewport mantiene relación de aspecto
+        viewport = new FitViewport(game.V_WIDTH, game.V_HEIGHT, camera);
+
+        // Ajusta la cámara a la vista
+        stage = new Stage(viewport, game.batcher);
+        Gdx.input.setInputProcessor(stage);
+
+        buildUI();
+    }
+
+    /** Construye la UI o sprites, botones, etc. */
+    protected abstract void buildUI();
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl.glClearColor(0,0,0,0);
+
+        viewport.apply();
+
+        camera.update();
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
+
+    @Override public void pause() { }
+
+    @Override public void resume() { }
+
+    @Override public void hide() { }
+
+    @Override public void dispose() {
+        stage.dispose();
+    }
 }
--- core/src/main/java/com/alon/pruebasGDX/screens/MainMenuScreen.java
+++ core/src/main/java/com/alon/pruebasGDX/screens/MainMenuScreen.java
@@ -1,15 +1,24 @@
-package com.alon.pruebasGDX;
+package com.alon.pruebasGDX.screens;
 
+import com.alon.pruebasGDX.assets.Assets;
+import com.alon.pruebasGDX.Prueba1;
+import com.alon.pruebasGDX.utils.Settings;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class MainMenuScreen extends ScreenAdapter {
+public class MainMenuScreen extends BaseScreen {
     Prueba1 game;
-    ScreenViewport viewport;
-    OrthographicCamera guiCamera;
 //    Rectangle soundBounds;
 //    Rectangle playBounds;
 //    Rectangle highscoresBounds;
@@ -17,16 +26,46 @@ public class MainMenuScreen extends ScreenAdapter {
 //    Vector3 touchPoint;
 
     public MainMenuScreen (Prueba1 game) {
+        super(game);
         this.game = game;
 
-        guiCamera = new OrthographicCamera(800, 500);
-        viewport = new ScreenViewport(guiCamera);
-        guiCamera.position.set((float) 320 / 2, (float) 480 / 2, 0);
 //        soundBounds = new Rectangle(0, 0, 64, 64);
 //        playBounds = new Rectangle(160 - 150, 200 + 18, 300, 36);
 //        highscoresBounds = new Rectangle(160 - 150, 200 - 18, 300, 36);
 //        helpBounds = new Rectangle(160 - 150, 200 - 18 - 36, 300, 36);
 //        touchPoint = new Vector3();
+    }
+
+    @Override
+    protected void buildUI() {
+        Table mainTable = new Table();
+        mainTable.setFillParent(true);
+        stage.addActor(mainTable);
+
+//        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+//
+//        final Label link = new Label("Haz clic aquí", skin);
+//        link.setColor(Color.WHITE);
+//        link.setPosition(100, 200);
+//
+//        // Añade un listener que intercambie el color al entrar/salir y gestione el clic
+//        link.addListener(new ClickListener() {
+//            @Override
+//            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+//                link.setColor(Color.CYAN);  // color on hover
+//                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand); // opcional: cursor mano
+//            }
+//            @Override
+//            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+//                link.setColor(Color.WHITE); // vuelve al color normal
+//                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow); // opcional: cursor flecha
+//            }
+//            @Override
+//            public void clicked(InputEvent event, float x, float y) {
+//                Gdx.app.exit();
+//            }
+//        });
+
     }
 
     public void update () {
@@ -63,22 +102,29 @@ public class MainMenuScreen extends ScreenAdapter {
         GL20 gl = Gdx.gl;
         gl.glClearColor(1, 0, 0, 1);
         gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        guiCamera.update();
-        game.batcher.setProjectionMatrix(guiCamera.combined);
+        camera.update();
+        game.batcher.setProjectionMatrix(camera.combined);
 
         game.batcher.disableBlending();
         game.batcher.begin();
-        game.batcher.draw(Assets.backgroundTexture, 0, 0, 320, 480);
+        game.batcher.draw(Assets.backgroundTexture,0,0, game.V_WIDTH, game.V_HEIGHT);
+//        game.batcher.draw(Assets.backgroundTexture, (float) game.V_WIDTH /2 - 150, (float) game.V_HEIGHT /2 - 200, 300, 400);
         game.batcher.end();
 
         game.batcher.enableBlending();
         game.batcher.begin();
-        game.batcher.draw(Assets.logo, 160 - 274 / 2, 480 - 10 - 142, 274, 142);
+//        game.batcher.draw(Assets.logo, 160 - 274 / 2, 480 - 10 - 142, 274, 142);
 //        game.batcher.draw(Assets.mainMenu, 10, 200 - 110 / 2, 300, 110);
 //        game.batcher.draw(Settings.soundEnabled ? Assets.soundOn : Assets.soundOff, 0, 0, 64, 64);
         game.batcher.end();
     }
 
+    @Override
+    public void show() {
+        Assets.mainMusic.setLooping(true);
+        Assets.mainMusic.setVolume(0.05f);
+        Assets.mainMusic.play();
+    }
     @Override
     public void render (float delta) {
         update();
