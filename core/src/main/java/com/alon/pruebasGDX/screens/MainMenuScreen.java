8b7fb2720cfdbf0786074aa15db600e4209c432a
Alon <alonso.moros@alumnos.upm.es>
2025-05-04 10:05:58 +0200

    Añadido JSON para el boton de prueba

--- core/src/main/java/com/alon/pruebasGDX/screens/MainMenuScreen.java
+++ core/src/main/java/com/alon/pruebasGDX/screens/MainMenuScreen.java
@@ -5,12 +5,11 @@ import com.alon.pruebasGDX.Prueba1;
 import com.alon.pruebasGDX.utils.Settings;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Cursor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
@@ -24,6 +23,8 @@ public class MainMenuScreen extends BaseScreen {
 //    Rectangle highscoresBounds;
 //    Rectangle helpBounds;
 //    Vector3 touchPoint;
+    private Texture background;
+    private Music music;
 
     public MainMenuScreen (Prueba1 game) {
         super(game);
@@ -40,31 +41,40 @@ public class MainMenuScreen extends BaseScreen {
     protected void buildUI() {
         Table mainTable = new Table();
         mainTable.setFillParent(true);
+
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
+        final Label link = new Label("< Nueva Partida >", skin);
+        link.setColor(Color.WHITE);
+        link.setFontScale(1.5f);
+        link.setPosition(333, 250);
+
+        // Añade un listener que intercambie el color al entrar/salir y gestione el clic
+        link.addListener(new ClickListener() {
+            @Override
+            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
+                link.setColor(Color.CYAN);  // color on hover
+                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand); // opcional: cursor mano
+            }
+            @Override
+            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
+                link.setColor(Color.WHITE); // vuelve al color normal
+                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow); // opcional: cursor flecha
+            }
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Assets.playSound(Assets.getSound(Assets.LEVEL_UP_SOUND));
+//                Gdx.app.exit();
+            }
+        });
+
+        stage.addActor(link);
         stage.addActor(mainTable);
 
-//        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
-//
-//        final Label link = new Label("Haz clic aquí", skin);
-//        link.setColor(Color.WHITE);
-//        link.setPosition(100, 200);
-//
-//        // Añade un listener que intercambie el color al entrar/salir y gestione el clic
-//        link.addListener(new ClickListener() {
-//            @Override
-//            public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
-//                link.setColor(Color.CYAN);  // color on hover
-//                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Hand); // opcional: cursor mano
-//            }
-//            @Override
-//            public void exit(InputEvent event, float x, float y, int pointer, Actor toActor) {
-//                link.setColor(Color.WHITE); // vuelve al color normal
-//                Gdx.graphics.setSystemCursor(Cursor.SystemCursor.Arrow); // opcional: cursor flecha
-//            }
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                Gdx.app.exit();
-//            }
-//        });
+//        Button b = new Button(skin);
+//        mainTable.add(b).center().width(300).height(100);
+
+        Gdx.input.setInputProcessor(stage);
 
     }
 
@@ -107,23 +117,24 @@ public class MainMenuScreen extends BaseScreen {
 
         game.batcher.disableBlending();
         game.batcher.begin();
-        game.batcher.draw(Assets.backgroundTexture,0,0, game.V_WIDTH, game.V_HEIGHT);
-//        game.batcher.draw(Assets.backgroundTexture, (float) game.V_WIDTH /2 - 150, (float) game.V_HEIGHT /2 - 200, 300, 400);
+        background = Assets.getTexture(Assets.BACKGROUND);
+        game.batcher.draw(background,0,0, game.V_WIDTH, game.V_HEIGHT);
         game.batcher.end();
 
         game.batcher.enableBlending();
         game.batcher.begin();
-//        game.batcher.draw(Assets.logo, 160 - 274 / 2, 480 - 10 - 142, 274, 142);
-//        game.batcher.draw(Assets.mainMenu, 10, 200 - 110 / 2, 300, 110);
-//        game.batcher.draw(Settings.soundEnabled ? Assets.soundOn : Assets.soundOff, 0, 0, 64, 64);
         game.batcher.end();
+
+        stage.draw();
+        stage.act();
     }
 
     @Override
     public void show() {
-        Assets.mainMusic.setLooping(true);
-        Assets.mainMusic.setVolume(0.05f);
-        Assets.mainMusic.play();
+        music = Assets.getMusic(Assets.MUSIC);
+        music.setLooping(true);
+        music.setVolume(0.05f);
+        music.play();
     }
     @Override
     public void render (float delta) {
