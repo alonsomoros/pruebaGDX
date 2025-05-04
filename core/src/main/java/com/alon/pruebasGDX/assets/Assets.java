dd8212bf3d917cf7bdf508554879de677a7bb891
Alon <alonso.moros@alumnos.upm.es>
2025-05-04 10:02:40 +0200

    Implementado Asset Manager para mayor velocidad de carga y control de assets

--- core/src/main/java/com/alon/pruebasGDX/assets/Assets.java
+++ core/src/main/java/com/alon/pruebasGDX/assets/Assets.java
@@ -2,43 +2,80 @@ package com.alon.pruebasGDX.assets;
 
 import com.alon.pruebasGDX.utils.Settings;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Assets {
-    public static Texture logo;
 
-    public static Texture backgroundTexture;
-    public static Sprite backgroundSprite;
+    public static final AssetManager assetManager = new AssetManager();
 
-    public static Texture figureTexture;
-    public static Sprite figureSprite;
+    public static final String
+        LOGO = "sprites/arquero/arquero_lvl3.png",
+        BACKGROUND = "background/tienda.png",
+        FIGURA_MAGO_3 = "sprites/mago/mago_lvl3.png",
+        MUSIC = "music/DanceOf1000Suns.wav",
+        FIRBALL_SOUND_1 = "sounds/fireballSound1.mp3",
+        FIRBALL_SOUND_2 = "sounds/fireballSound2.mp3",
+        LEVEL_UP_SOUND = "sounds/levelUpSound2.mp3";
 
-    public static Music mainMusic;
 
-    public static Sound fireballSound1;
-    public static Sound fireballSound2;
-    public static Sound levelUpSound;
+    //public static Texture logo;
+    //public static Texture backgroundTexture;
+    //public static Texture figureTexture;
+
+    //public static Music mainMusic;
+
+    //public static Sound fireballSound1;
+    //public static Sound fireballSound2;
+    //public static Sound levelUpSound;
 
     public static Texture loadTexture (String file) {
         return new Texture(Gdx.files.internal(file));
     }
 
     public static void load() {
-        logo = new Texture(Gdx.files.internal("background/tienda.png"));
 
-        backgroundTexture = loadTexture("background/tienda.png");
-        backgroundSprite = new Sprite(backgroundTexture);
+        // logo = new Texture(Gdx.files.internal("icons/LogoSeaOfStars_Black.png"));
+        assetManager.load(LOGO, Texture.class);
 
-        figureTexture = loadTexture("sprites/mago/mago_lvl3.png");
-        figureSprite = new Sprite(figureTexture);
+        // backgroundTexture = loadTexture("background/tienda.png");
+        assetManager.load(BACKGROUND, Texture.class);
 
-        mainMusic = Gdx.audio.newMusic(Gdx.files.internal("music/DanceOf1000Suns.wav"));
-        fireballSound1 = Gdx.audio.newSound(Gdx.files.internal("sounds/fireballSound1.mp3"));
-        fireballSound2 = Gdx.audio.newSound(Gdx.files.internal("sounds/fireballSound2.mp3"));
-        levelUpSound = Gdx.audio.newSound(Gdx.files.internal("sounds/levelUpSound.mp3"));
+        // figureTexture = loadTexture("sprites/mago/mago_lvl3.png");
+        assetManager.load(FIGURA_MAGO_3, Texture.class);
+
+        // mainMusic = Gdx.audio.newMusic(Gdx.files.internal("music/DanceOf1000Suns.wav"));
+        assetManager.load(MUSIC, Music.class);
+        // fireballSound1 = Gdx.audio.newSound(Gdx.files.internal("sounds/fireballSound1.mp3"));
+        assetManager.load(FIRBALL_SOUND_1, Sound.class);
+        // fireballSound2 = Gdx.audio.newSound(Gdx.files.internal("sounds/fireballSound2.mp3"));
+        assetManager.load(FIRBALL_SOUND_2, Sound.class);
+        // levelUpSound = Gdx.audio.newSound(Gdx.files.internal("sounds/levelUpSound.mp3"));
+        assetManager.load(LEVEL_UP_SOUND, Sound.class);
+        // … más assets
+    }
+
+    public static void finishLoading() {
+        assetManager.finishLoading(); // bloqueante
+        Gdx.app.log("Assets", "Cargados " + assetManager.getLoadedAssets() + " assets");
+    }
+
+    public static Texture getTexture(String name) {
+        return assetManager.get(name, Texture.class);
+    }
+
+    public static Sound getSound(String name) {
+        return assetManager.get(name, Sound.class);
+    }
+
+    public static Music getMusic(String name) {
+        return assetManager.get(name, Music.class);
+    }
+
+    public static void dispose() {
+        assetManager.dispose();
     }
 
     public static void playSound (Sound sound) {
