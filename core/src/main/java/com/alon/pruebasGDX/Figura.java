7692a57d22194a3ba37a3af7531d78f4884f95b7
Alon <alonsomorosvillalba@gmail.com>
2025-05-09 01:57:01 +0200

    Añadido bajarNivel en Figura

--- core/src/main/java/com/alon/pruebasGDX/Figura.java
+++ core/src/main/java/com/alon/pruebasGDX/Figura.java
@@ -62,4 +62,42 @@ public class Figura {
         this.sprite.setPosition(oldX, oldY);
         this.sprite.setSize(125f, 145f);
     }
+
+    public void bajarNivel() {
+        float oldX = 0;
+        float oldY = 0;
+
+        // Guardar posición actual si el sprite existe
+        if (sprite != null) {
+            oldX = sprite.getX();
+            oldY = sprite.getY();
+        }
+
+        this.nivel--;
+        Gdx.app.log("Nivel", nivel + " :(");
+
+        if (nivel < 1) {
+            nivel = 1;
+        }
+
+        if (sprite == null) {
+            sprite = new Sprite(Assets.loadTexture(Assets.FIGURA_MAGO_1));
+        } else {
+            switch (nivel) {
+                case 1:
+                    this.sprite = new Sprite(Assets.loadTexture(Assets.FIGURA_MAGO_1));
+                    break;
+                case 2:
+                    this.sprite = new Sprite(Assets.loadTexture(Assets.FIGURA_MAGO_2));
+                    break;
+                case 3:
+                    this.sprite = new Sprite(Assets.loadTexture(Assets.FIGURA_MAGO_3));
+                    break;
+            }
+        }
+
+        // Restaurar posición y establecer tamaño constante
+        this.sprite.setPosition(oldX, oldY);
+        this.sprite.setSize(125f, 145f);
+    }
 }
