43c0590f718163099d004f1dde90941863cf8960
Alon <alonsomorosvillalba@gmail.com>
2025-05-11 23:34:15 +0200

    Implementado el resto de Assets en el load

--- core/src/main/java/com/alon/pruebasGDX/assets/Assets.java
+++ core/src/main/java/com/alon/pruebasGDX/assets/Assets.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 
+import java.io.File;
+
 public class Assets {
 
     public static final AssetManager assetManager = new AssetManager();
@@ -64,7 +66,7 @@ public class Assets {
         ACTIVADOR_ROMBO_5_PATH = "sprites/activadores/ActivadorRombo_5.png",
 
     // SPRITES -> ATAQUES
-    ATAQUE_ARQUERO_PATH = "sprites/ataques/ataqueArquero.png",
+        ATAQUE_ARQUERO_PATH = "sprites/ataques/ataqueArquero.png",
         ATAQUE_ASESINO_PATH = "sprites/ataques/ataqueAsesino.png",
         ATAQUE_CABALLERO_PATH = "sprites/ataques/ataqueCaballero.png",
         ATAQUE_INGENIERO_PATH = "sprites/ataques/ataqueIngeniero.png",
@@ -104,23 +106,23 @@ public class Assets {
         FIGURA_ARQUERO_2_PATH = "sprites/figuras/arquero/arquero_lvl2.png",
         FIGURA_ARQUERO_3_PATH = "sprites/figuras/arquero/arquero_lvl3.png",
 
-    FIGURA_ASESINO_1_PATH = "sprites/figuras/asesino/asesino_lvl1.png",
+        FIGURA_ASESINO_1_PATH = "sprites/figuras/asesino/asesino_lvl1.png",
         FIGURA_ASESINO_2_PATH = "sprites/figuras/asesino/asesino_lvl2.png",
         FIGURA_ASESINO_3_PATH = "sprites/figuras/asesino/asesino_lvl3.png",
 
-    FIGURA_CABALLERO_1_PATH = "sprites/figuras/caballero/caballero_lvl1.png",
+        FIGURA_CABALLERO_1_PATH = "sprites/figuras/caballero/caballero_lvl1.png",
         FIGURA_CABALLERO_2_PATH = "sprites/figuras/caballero/caballero_lvl2.png",
         FIGURA_CABALLERO_3_PATH = "sprites/figuras/caballero/caballero_lvl3.png",
 
-    FIGURA_INGENIERO_1_PATH = "sprites/figuras/ingeniero/ingeniero_lvl1.png",
+        FIGURA_INGENIERO_1_PATH = "sprites/figuras/ingeniero/ingeniero_lvl1.png",
         FIGURA_INGENIERO_2_PATH = "sprites/figuras/ingeniero/ingeniero_lvl2.png",
         FIGURA_INGENIERO_3_PATH = "sprites/figuras/ingeniero/ingeniero_lvl3.png",
 
-    FIGURA_MAGO_1_PATH = "sprites/figuras/mago/mago_lvl1.png",
+        FIGURA_MAGO_1_PATH = "sprites/figuras/mago/mago_lvl1.png",
         FIGURA_MAGO_2_PATH = "sprites/figuras/mago/mago_lvl2.png",
         FIGURA_MAGO_3_PATH = "sprites/figuras/mago/mago_lvl3.png",
 
-    FIGURA_SACERDOTE_1_PATH = "sprites/figuras/sacerdote/sacerdote_lvl1.png",
+        FIGURA_SACERDOTE_1_PATH = "sprites/figuras/sacerdote/sacerdote_lvl1.png",
         FIGURA_SACERDOTE_2_PATH = "sprites/figuras/sacerdote/sacerdote_lvl2.png",
         FIGURA_SACERDOTE_3_PATH = "sprites/figuras/sacerdote/sacerdote_lvl3.png";
 
@@ -139,7 +141,7 @@ public class Assets {
         CUADRADO_NIVEL_LATERAL_2_PATH = "sprites/rodillo/cuadradoNivel2Lateral.png",
         CUADRADO_NIVEL_LATERAL_3_PATH = "sprites/rodillo/cuadradoNivel3Lateral.png",
 
-    ROMBO_NORMAL_1_PATH = "sprites/rodillo/rombo1.png",
+        ROMBO_NORMAL_1_PATH = "sprites/rodillo/rombo1.png",
         ROMBO_NORMAL_2_PATH = "sprites/rodillo/rombo2.png",
         ROMBO_NORMAL_3_PATH = "sprites/rodillo/rombo3.png",
         ROMBO_NORMAL_LATERAL_1_PATH = "sprites/rodillo/rombo1Lateral.png",
@@ -152,17 +154,17 @@ public class Assets {
         ROMBO_NIVEL_LATERAL_2_PATH = "sprites/rodillo/romboNivel2Lateral.png",
         ROMBO_NIVEL_LATERAL_3_PATH = "sprites/rodillo/romboNivel3Lateral.png",
 
-    MARTILLO_1_PATH = "sprites/rodillo/martillo1.png",
+        MARTILLO_1_PATH = "sprites/rodillo/martillo1.png",
         MARTILLO_2_PATH = "sprites/rodillo/martillo2.png",
         MARTILLO_3_PATH = "sprites/rodillo/martillo3.png",
         MARTILLO_LATERAL_1_PATH = "sprites/rodillo/martillo1Lateral.png",
         MARTILLO_LATERAL_2_PATH = "sprites/rodillo/martillo2Lateral.png",
         MARTILLO_LATERAL_3_PATH = "sprites/rodillo/martillo3Lateral.png",
 
-    ROTO_PATH = "sprites/rodillo/roto.png",
+        ROTO_PATH = "sprites/rodillo/roto.png",
         ROTO_LATERAL_PATH = "sprites/rodillo/rotoLateral.png",
 
-    GIRARRODILLO_PATH = "sprites/rodillo/girarrodillo.png",
+        GIRARRODILLO_PATH = "sprites/rodillo/girarrodillo.png",
         FIJADOR_PATH = "sprites/rodillo/fijador.png";
 
     // SPRITES -> STATS_FIGURA
@@ -188,11 +190,6 @@ public class Assets {
         WATERBALL_ATLAS_PATH = "sprites/waterball/waterball.atlas",
         WATERBALL_SPRITESHEET_PATH = "sprites/waterball/waterball.png";
 
-    // TEXT
-    public static final String
-        INSTRUCCIONES_PATH = "text/instrucciones.txt",
-        CREDITOS_PATH = "text/creditos.txt";
-
     public static Texture loadTexture(String file) {
         return new Texture(Gdx.files.internal(file));
     }
@@ -209,6 +206,21 @@ public class Assets {
         assetManager.load(FIGURA_MAGO_1_PATH, Texture.class);
         assetManager.load(FIGURA_MAGO_2_PATH, Texture.class);
         assetManager.load(FIGURA_MAGO_3_PATH, Texture.class);
+        assetManager.load(FIGURA_ARQUERO_1_PATH, Texture.class);
+        assetManager.load(FIGURA_ARQUERO_2_PATH, Texture.class);
+        assetManager.load(FIGURA_ARQUERO_3_PATH, Texture.class);
+//        assetManager.load(FIGURA_ASESINO_1_PATH, Texture.class);
+//        assetManager.load(FIGURA_ASESINO_2_PATH, Texture.class);
+//        assetManager.load(FIGURA_ASESINO_3_PATH, Texture.class);
+//        assetManager.load(FIGURA_CABALLERO_1_PATH, Texture.class);
+//        assetManager.load(FIGURA_CABALLERO_2_PATH, Texture.class);
+//        assetManager.load(FIGURA_CABALLERO_3_PATH, Texture.class);
+//        assetManager.load(FIGURA_INGENIERO_1_PATH, Texture.class);
+//        assetManager.load(FIGURA_INGENIERO_2_PATH, Texture.class);
+//        assetManager.load(FIGURA_INGENIERO_3_PATH, Texture.class);
+        assetManager.load(FIGURA_SACERDOTE_1_PATH, Texture.class);
+        assetManager.load(FIGURA_SACERDOTE_2_PATH, Texture.class);
+        assetManager.load(FIGURA_SACERDOTE_3_PATH, Texture.class);
         assetManager.load(MAIN_MENU_MUSIC_PATH, Music.class);
         assetManager.load(MINIGAME_MUSIC_PATH, Music.class);
         assetManager.load(WHEELS_MUSIC_PATH, Music.class);
@@ -225,10 +237,89 @@ public class Assets {
         assetManager.load(FIREBALL_1_PATH, Texture.class);
         assetManager.load(FIREBALL_2_PATH, Texture.class);
         assetManager.load(FIREBALL_3_PATH, Texture.class);
+        assetManager.load(FIREBALL_SPRITESHEET_PATH, Texture.class);
         assetManager.load(WATERBALL_ATLAS_PATH, com.badlogic.gdx.graphics.g2d.TextureAtlas.class);
         assetManager.load(WATERBALL_1_PATH, Texture.class);
         assetManager.load(WATERBALL_2_PATH, Texture.class);
         assetManager.load(WATERBALL_3_PATH, Texture.class);
+        assetManager.load(WATERBALL_SPRITESHEET_PATH, Texture.class);
+        assetManager.load(STATS_VACIO_PATH, Texture.class);
+        assetManager.load(STATS_GUERRERROS_PATH, Texture.class);
+        assetManager.load(STATS_SACERDOTE_PATH, Texture.class);
+        assetManager.load(STATS_ASESINO_PATH, Texture.class);
+        assetManager.load(BASTION_1_PATH, Texture.class);
+        assetManager.load(BASTION_2_PATH, Texture.class);
+        assetManager.load(BASTION_3_PATH, Texture.class);
+        assetManager.load(BASTION_4_PATH, Texture.class);
+        assetManager.load(BASTION_5_PATH, Texture.class);
+        assetManager.load(ROMBO_NORMAL_1_PATH, Texture.class);
+        assetManager.load(ROMBO_NORMAL_2_PATH, Texture.class);
+        assetManager.load(ROMBO_NORMAL_3_PATH, Texture.class);
+        assetManager.load(ROMBO_NORMAL_LATERAL_1_PATH, Texture.class);
+        assetManager.load(ROMBO_NORMAL_LATERAL_2_PATH, Texture.class);
+        assetManager.load(ROMBO_NORMAL_LATERAL_3_PATH, Texture.class);
+        assetManager.load(ROMBO_NIVEL_1_PATH, Texture.class);
+        assetManager.load(ROMBO_NIVEL_2_PATH, Texture.class);
+        assetManager.load(ROMBO_NIVEL_3_PATH, Texture.class);
+        assetManager.load(ROMBO_NIVEL_LATERAL_1_PATH, Texture.class);
+        assetManager.load(ROMBO_NIVEL_LATERAL_2_PATH, Texture.class);
+        assetManager.load(ROMBO_NIVEL_LATERAL_3_PATH, Texture.class);
+        assetManager.load(CUADRADO_NORMAL_1_PATH, Texture.class);
+        assetManager.load(CUADRADO_NORMAL_2_PATH, Texture.class);
+        assetManager.load(CUADRADO_NORMAL_3_PATH, Texture.class);
+        assetManager.load(CUADRADO_NORMAL_LATERAL_1_PATH, Texture.class);
+        assetManager.load(CUADRADO_NORMAL_LATERAL_2_PATH, Texture.class);
+        assetManager.load(CUADRADO_NORMAL_LATERAL_3_PATH, Texture.class);
+        assetManager.load(CUADRADO_NIVEL_1_PATH, Texture.class);
+        assetManager.load(CUADRADO_NIVEL_2_PATH, Texture.class);
+        assetManager.load(CUADRADO_NIVEL_3_PATH, Texture.class);
+        assetManager.load(CUADRADO_NIVEL_LATERAL_1_PATH, Texture.class);
+        assetManager.load(CUADRADO_NIVEL_LATERAL_2_PATH, Texture.class);
+        assetManager.load(CUADRADO_NIVEL_LATERAL_3_PATH, Texture.class);
+        assetManager.load(MARTILLO_1_PATH, Texture.class);
+        assetManager.load(MARTILLO_2_PATH, Texture.class);
+        assetManager.load(MARTILLO_3_PATH, Texture.class);
+        assetManager.load(MARTILLO_LATERAL_1_PATH, Texture.class);
+        assetManager.load(MARTILLO_LATERAL_2_PATH, Texture.class);
+        assetManager.load(MARTILLO_LATERAL_3_PATH, Texture.class);
+        assetManager.load(ROTO_PATH, Texture.class);
+        assetManager.load(ROTO_LATERAL_PATH, Texture.class);
+        assetManager.load(GIRARRODILLO_PATH, Texture.class);
+        assetManager.load(FIJADOR_PATH, Texture.class);
+        assetManager.load(ATAQUE_ARQUERO_PATH, Texture.class);
+        assetManager.load(ATAQUE_ASESINO_PATH, Texture.class);
+        assetManager.load(ATAQUE_CABALLERO_PATH, Texture.class);
+        assetManager.load(ATAQUE_INGENIERO_PATH, Texture.class);
+        assetManager.load(ATAQUE_MAGO_PATH, Texture.class);
+        assetManager.load(ATAQUE_SACERDOTE_PATH, Texture.class);
+        assetManager.load(ATAQUE_ESPECIAL_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_0_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_1_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_1_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_2_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_3_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_4_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_5_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_CUADRADO_6_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_0_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_1_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_2_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_3_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_4_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_5_PATH, Texture.class);
+        assetManager.load(BASE_FIGURA_ROMBO_6_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_CUADRADO_0_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_CUADRADO_1_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_CUADRADO_2_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_CUADRADO_3_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_CUADRADO_4_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_CUADRADO_5_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_ROMBO_0_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_ROMBO_1_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_ROMBO_2_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_ROMBO_3_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_ROMBO_4_PATH, Texture.class);
+        assetManager.load(ACTIVADOR_ROMBO_5_PATH, Texture.class);
 
         // … más assets
         assetManager.finishLoading();
